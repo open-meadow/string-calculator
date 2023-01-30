26064dcd305124e8847745cb99abc50a4f8d8749
open-meadow <rohan.varughese@protonmail.com>
2023-01-30 19:23:22 +0000

    Add logic

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -14,11 +14,27 @@ describe StringCalculator do
         expect(StringCalculator.add("4")).to eql(4)
       end
     end
-    
+
     context "given 10" do
       it "returns 10" do
         expect(StringCalculator.add("10")).to eq(10)
       end
+    end
+
+    context "two numbers" do
+
+      context "given 2, 4" do
+        it "returns 6" do
+          expect(StringCalculator.add("2, 4")).to eq(6)
+        end
+      end
+
+      context "given 17, 100" do
+        it "returns 117" do
+          expect(StringCalculator.add("17, 100")).to eq(117)
+        end
+      end
+
     end
 
   end
